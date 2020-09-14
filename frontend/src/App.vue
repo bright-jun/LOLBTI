03cd1fb9ab16d5f5670c5dc394d22af89fbae946
ohgiseok <oks2238@gmail.com>
2020-09-14 22:50:17 +0900

    S03P22A109-58 프론트 틀 잡기 grid, color 수정 필요

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,29 @@
 <template>
-  <div id="app">
-    <router-view></router-view>
-  </div>
+  <v-app class="grey lighten-4">
+    <NavBar />
+    <Banner />
+    <v-main>
+      <router-view> </router-view>
+    </v-main>
+    <Footer />
+  </v-app>
 </template>
+
 <script>
+import NavBar from "./components/NavBar.vue";
+import Banner from "./components/Banner.vue";
+import Footer from "./components/Footer.vue";
 export default {
-  name: "app"
+  name: "App",
+
+  components: {
+    NavBar,
+    Banner,
+    Footer,
+  },
+
+  data: () => ({
+    //
+  }),
 };
 </script>
---
+++ frontend/src/components/NavBar.vue
@@ -0,0 +1,42 @@
+<template>
+  <nav>
+    <v-toolbar flat app class="maincolor">
+      <v-toolbar-title class="grey--text">
+        <span class="font-weight-light">lol</span>
+        <span>BTI</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-flex xs-4>
+        <v-form>
+          <v-text-field
+            outline
+            label="닉네임 검색"
+            append-icon="search"
+          ></v-text-field>
+        </v-form>
+      </v-flex>
+      <v-btn flat class="btncolor">
+        <span>로그인</span>
+        <v-icon right>login</v-icon>
+      </v-btn>
+      <!-- <v-btn flat color="grey">
+        <span>로그아웃</span>
+        <v-icon right>exit_to_app</v-icon>
+      </v-btn> -->
+      <v-btn flat class="btncolor">
+        <span>회원가입</span>
+        <!-- <v-icon right></v-icon> -->
+      </v-btn>
+    </v-toolbar>
+  </nav>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang=""></style>
