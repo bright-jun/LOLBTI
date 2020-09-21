56fdd2d3a86c40eace1dfeb8784804851f457b98
부권남 <lukas523@naver.com>
2020-09-21 11:35:13 +0900

    KN 로그인 수정

--- backend_spring/src/main/java/com/web/curation/controller/UserController.java
+++ backend_spring/src/main/java/com/web/curation/controller/UserController.java
@@ -3,19 +3,18 @@ package com.web.curation.controller;
 import java.util.List;
 import java.util.Optional;
 
-
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -85,9 +84,19 @@ public class UserController {
     
     @PostMapping("/user/insert")
     @ApiOperation(value = "회원 가입")
-    public ResponseEntity<User> save(User user){
-    	//BasicResponse result = new BasicResponse(); 
-    	return new ResponseEntity<User>(userService.save(user), HttpStatus.OK);
+    public Object save(User user){
+    	BasicResponse result = new BasicResponse();
+    	Optional<User> userOpt = userService.findById(user.getId());
+    	
+    	// ID가 이미 존재하지 않을때만 회원가입 가능?
+    	if (!userOpt.isPresent()) {
+    		result.status = true;
+            result.object = userOpt.get();
+    		return new ResponseEntity<>(result, HttpStatus.OK);
+    	}else {
+    		result.status = false;
+    		return new ResponseEntity<>(result, HttpStatus.NOT_FOUND);
+    	}
     }
     
  	@PutMapping("/user/update/{id}")
@@ -104,7 +113,4 @@ public class UserController {
  		return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
  	}
 
- 	
-
-    
 }
