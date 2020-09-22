7afd4ab3dd5326816a223ff9c98ee6df7fa31555
ohgiseok <oks2238@gmail.com>
2020-09-22 22:38:40 +0900

    S03P23A109-29 소환사 이름 VUEX 에 저장까지

--- frontend/src/views/Welcome.vue
+++ frontend/src/views/Welcome.vue
@@ -2,21 +2,16 @@
   <v-main>
     <nav-bar :viewType="navbarType" />
     <v-container>
-      <v-img
-        class="ma-auto"
-        src="../assets/images/lolbti_logo_greenblack.png"
-        height="180"
-        width="310"
-      />
+      <v-img class="ma-auto" src="../assets/images/lolbti_logo_2.png" height="180" width="310" />
       <v-row class="pt-5">
         <v-col md="3"></v-col>
         <v-col xs="11" md="5">
           <v-form>
-            <v-text-field outline label="닉네임 검색" append-icon="search"></v-text-field>
+            <v-text-field outline label="닉네임 검색" append-icon="search" v-model="summonerName"></v-text-field>
           </v-form>
         </v-col>
         <v-col xs="1" md="1" class="pt-5">
-          <v-btn class="btncolor">검색</v-btn>
+          <v-btn class="btncolor" @click="submit">검색</v-btn>
         </v-col>
         <v-col md="3"></v-col>
       </v-row>
@@ -26,15 +21,27 @@
 
 <script>
 import NavBar from "../components/NavBar.vue";
+import { EventBus } from "../main.js";
 
 export default {
   data() {
     return {
+      summonerName: "",
+      searchType: 1,
       navbarType: false,
     };
   },
   components: {
     NavBar,
+  },
+  methods: {
+    submit() {
+      this.$store.state.summoner = {
+        name: this.summonerName,
+        searchType: this.searchType,
+      };
+      this.$router.push("/home");
+    },
   },
 };
 </script>
