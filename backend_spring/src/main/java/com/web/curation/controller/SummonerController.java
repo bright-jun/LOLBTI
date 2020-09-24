6fb1b3c4f5ec6363acc55b0e7e5591fa06cbcff4
son <sondr1004@naver.com>
2020-09-24 18:36:32 +0900

    유저랭크(Spring)

--- backend_spring/src/main/java/com/web/curation/controller/SummonerController.java
+++ backend_spring/src/main/java/com/web/curation/controller/SummonerController.java
@@ -1,13 +1,12 @@
 package com.web.curation.controller;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.IOException;
 
+import com.web.curation.dao.user.UserRankDao;
 import com.web.curation.model.BasicResponse;
-import com.web.curation.model.lol.Champion;
-import com.web.curation.model.lol.Item;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -18,21 +17,34 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 
-
 @ApiResponses(value = { @ApiResponse(code = 401, message = "Unauthorized", response = BasicResponse.class),
         @ApiResponse(code = 403, message = "Forbidden", response = BasicResponse.class),
         @ApiResponse(code = 404, message = "Not Found", response = BasicResponse.class),
         @ApiResponse(code = 500, message = "Failure", response = BasicResponse.class) })
 
-// @CrossOrigin(origins = { "https://i3a310.p.ssafy.io:80", "http://localhost:3000" })
+// @CrossOrigin(origins = { "https://i3a310.p.ssafy.io:80",
+// "http://localhost:3000" })
 @CrossOrigin(origins = { "*" })
 @RestController
 public class SummonerController {
-    
+
+    @Autowired
+    UserRankDao userRankDao;
+
     @GetMapping("/summoner/recentchamp")
     @ApiOperation(value = "summoner 정보 가져오기 ")
     public Object recommendChampion(@RequestParam String summonerName) {
-     
-    	return null;
+
+        return null;
+    }
+
+    @GetMapping("/summoner")
+    @ApiOperation(value = "summoner 정보 가져오기 ")
+    public Object summonerInfo(@RequestParam String summonerName) throws IOException {
+        String userInfo = userRankDao.userRank(summonerName);
+        if(userInfo != null){
+            return new ResponseEntity<>(userInfo, HttpStatus.OK);
+        }
+    	return new ResponseEntity<>("fail", HttpStatus.NOT_FOUND);
     }
 }
