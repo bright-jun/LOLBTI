5c7f852520aa7b96c5f8ca8fc84591781052040a
부권남 <lukas523@naver.com>
2020-09-30 17:09:17 +0900

    KN django 배포, settings 추가

--- backend_spring/src/main/java/com/web/curation/dao/user/UserRankDaoImpl.java
+++ backend_spring/src/main/java/com/web/curation/dao/user/UserRankDaoImpl.java
@@ -5,19 +5,22 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLEncoder;
 
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class UserRankDaoImpl implements UserRankDao {
     static String root = "http://localhost:8081";
-    // static String root = "http://j3a109.p.ssafy.io:8081";
+    //static String root = "http://j3a109.p.ssafy.io:8081";
 
     @Override
     public String userRank(String summonerName) throws IOException {
         String request = "/userGameData/userinfo/";
-        String requestUrl = root + request + summonerName;
-        URL url = new URL(requestUrl);
+        String requestUrl = root + request;
+        String summon= URLEncoder.encode(summonerName, "UTF-8");
+        URL url = new URL(requestUrl+summon);
+        
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
         conn.setRequestMethod("GET");
 
