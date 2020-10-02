606577aafc85afd4b4c339f5ba38356dd59d28cf
bright-jun <brightj529@gmail.com>
2020-10-02 14:43:03 +0900

    key

--- backend_django/lolBTI/userGameData/setting.py
+++ backend_django/lolBTI/userGameData/setting.py
@@ -1,12 +1,9 @@
 api_key_list = [
-    'RGAPI-d3a53c22-f940-40b8-9e4c-089ad160d8f2',
-    'RGAPI-2866c236-5976-4cac-8955-49fccb487e8b',
-    'RGAPI-1389cd81-c02e-4fa1-9654-80579633a8ff',
-    'RGAPI-24091344-7123-4eac-8ebc-9f9c6e75311a',
-    'RGAPI-754d8513-daaf-4192-831f-a6953405e338',
-    'RGAPI-774ab6ec-eccf-4e33-8fce-27065d9e2a4c',
-    'RGAPI-dc94fdf0-94aa-4697-bf21-092ded3f9f18',
-    'RGAPI-914c67f1-e069-431b-833f-4262481851d2'
+    'RGAPI-e2156167-c88a-4fd4-8012-83aad126c933',
+    'RGAPI-c1a1f277-1c25-4b8b-b803-9755439763d9',
+    'RGAPI-b1272ff0-5a2c-4819-81c9-96e3e8417963',
+    'RGAPI-911ed4a1-b7b7-466d-8ffa-3dd532c5ee4b',
+    'RGAPI-edc57be5-357d-40dd-8a5a-c01966c180d9',
 ]
 
 sohwan_mastery = None
