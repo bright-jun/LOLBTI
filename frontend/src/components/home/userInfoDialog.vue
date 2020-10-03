4af820df8c734e5d5349618113bbe9bae6eed140
ohgiseok <oks2238@gmail.com>
2020-10-03 15:31:36 +0900

    ãS03P23A109 Develop 머지용

--- frontend/src/components/home/userInfoDialog.vue
+++ frontend/src/components/home/userInfoDialog.vue
@@ -52,6 +52,8 @@
                   label="MBTI 유형"
                 ></v-autocomplete>
               </v-col>
+            </v-row>
+            <v-row>
               <v-col>
                 <a
                   href="https://www.16personalities.com/ko/%EB%AC%B4%EB%A3%8C-%EC%84%B1%EA%B2%A9-%EC%9C%A0%ED%98%95-%EA%B2%80%EC%82%AC"
